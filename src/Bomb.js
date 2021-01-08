bd83bfe3680acbd800afa8e63fc1cb1e25ca5343
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2021-01-08 12:03:20 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,21 +1,3 @@
 // your Bomb code here!
   
-import React, { Component } from 'react';
-
-export default class Bomb extends Component {
-  
-  constructor(props) {
-    super(props)
-    this.state = {
-      secondsLeft: props.initialCount
-    }
-  }
-  
-  render() {
-    if(this.state.secondsLeft===0)
-     {return "Boom!"} 
-     else {return <div>{this.state.secondsLeft} seconds left before I go boom!</div>}
-    )
-  }
-  
-}
+import React from 'react';export default class Bomb extends React.Component { constructor(props) { super() this.state = { secondsLeft: props.initialCount } } render() { if(this.state.secondsLeft===0){ return 'Boom!' }else{ return <div>{this.state.secondsLeft} seconds left before I go boom!</div> } } }
